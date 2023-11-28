03c8b7535009ce21a9e1967441a9e5028b710745
fpdecastro97 <francisco.de.castro@mi.unc.edu.ar>
2023-11-28 20:31:23 +0100

    More accurancy to the file name of dowload

--- app3.py
+++ app3.py
@@ -157,6 +157,6 @@ def main():
 
     csv_data = df_stores.to_csv(index=False)
     b64 = base64.b64encode(csv_data.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="exported_data.csv">Descargar CSV</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="exported_{selected_filter}.csv">Descargar CSV</a>'
     st.markdown("<div style='height:40px'>",unsafe_allow_html=True)
     st.markdown(href,unsafe_allow_html=True)
